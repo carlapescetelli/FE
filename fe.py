091fad166e2f480efbba349819951cae51a6c59d
carlapescetelli <138565848+carlapescetelli@users.noreply.github.com>
2023-10-21 11:20:53 +0200

    Update fe.py

--- fe.py
+++ fe.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     oggetti=os.listdir('/content/MeshCNN/checkpoints/ogg/meshes/')
     with open('features.txt', 'w') as file:
         for el in oggetti:
-          mesh= Mesh(el, opt)
+          mesh= Mesh('/content/MeshCNN/checkpoints/ogg/meshes/'+ el, opt)
           edge_features = mesh.extract_features()
           file.write(edge_features+'\n')
     file.close()
